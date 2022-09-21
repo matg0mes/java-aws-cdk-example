b843d5d1b5ae59373d201c4466aff1c23a567c83
matg0mes <hotmus8500@gmail.com>
2022-09-20 23:37:25 -0300

    feat(app): start project e basic structure

---
+++ src/main/java/com/myorg/AwsCdkApp.java
@@ -0,0 +1,21 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+import java.util.Arrays;
+
+public class AwsCdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
+
+        ClusterStack clusterStack= new ClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        app.synth();
+    }
+}
+
